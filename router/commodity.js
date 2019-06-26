0d240edf831b253cf86fbd76cd835e27aa6f8d0a
web <j_duang@163.com>
2019-06-26 20:44:22 +0800

    bauble 的数据接口

--- router/commodity.js
+++ router/commodity.js
@@ -89,17 +89,18 @@ router.post('/details', (req, res) => {
     }
 });
 
-//根据session中的userId获取购物车列表
-router.post('/shoppingCart', (req, res) => {
+//根据session中的userId获取收藏夹列表
+router.post('/favorite', (req, res) => {
     let data = req.body;
-    let commodityId = parseInt(data.commodityId);
+    let userId = parseInt(data.userId);
     let sendData = {};
-    let sql = 'select `id`, `typeId`,`title`, `price`, `image`,`details`,`realLink` from commodity where id=?';
+    //select f.id favoriteId, c.typeId,title, c.price, c.image,c.details,c.realLink from commodity c join commodity_favorite f on c.id = f.commodityId where userId=1;
+    let sql = 'select f.id favoriteId, c.typeId,title, c.price, c.image,c.details,c.realLink from commodity c join commodity_favorite f on c.id = f.commodityId where userId=?';
     try {
-        pool.query(sql, [commodityId], (err, result) => {
+        pool.query(sql, [userId], (err, result) => {
             if (result.length > 0) {
                 sendData.details = result[0];
-                sql = 'select `id`, `typeId`,`title`, `price`, `image` from commodity where typeId = ? ORDER BY RAND() limit 0,4';
+                sql = 'select `id`, `typeId`,`title`, `price`, `image` from commodity where typeId in (?) ORDER BY RAND() limit 0,4';
                 try {
                     pool.query(sql, [sendData.details.typeId], (err, result) => {
                         if (result.length > 0) {
@@ -119,5 +120,18 @@ router.post('/shoppingCart', (req, res) => {
     } catch (e) {
         res.send({code: 500, msg: '服务器内部错误'});
     }
+});
+
+router.post('addFavorite', (req, res) => {
+    let data = req.body;
+    let userId = data.userId;
+    let sql = `insert into user (commodityId,userId,time) values (?,?,?)`;
+    pool.query(sql, [data.commodityId, data.userId, new Date().getTime()], (err, result) => {
+        if (result.affectedRows > 0) {
+            res.send({code: 200, msg: '注册成功'})
+        } else {
+            res.send({code: 201, msg: '注册失败'})
+        }
+    })
 })
 module.exports = router;
